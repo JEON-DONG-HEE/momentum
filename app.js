94958a720ed19b388dc53b977b4d14db71b7fdcb
JEON-DONG-HEE <bethetopone@gmail.com>
2021-06-26 14:55:08 +0900

    210626_02 영문 이름 업데이트

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ console.log(a * b);
 console.log(a / b);
 console.log("My name is " + myName);
 
-myName = "전국정";
+myName = "Jhon";
 
-console.log("나의 이전 이름은 " + myName);
+console.log("나의 영어 이름은 " + myName);
 
 // const 를 위주로 쓰고, let 은 가끔 필요할 때 쓰고, var 는 절대 안쓴다
