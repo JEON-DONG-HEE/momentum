78bf1e2063b78560318c56ea34ebf2a72cdddda5
JEON-DONG-HEE <bethetopone@gmail.com>
2021-06-26 14:53:30 +0900

    210626 const 와 let 의 차이와 쓰는 이유

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-alert('hi');
+const a = 50;
+const b = 2;
+let myName = "전동희";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("My name is " + myName);
+
+myName = "전국정";
+
+console.log("나의 이전 이름은 " + myName);
+
+// const 를 위주로 쓰고, let 은 가끔 필요할 때 쓰고, var 는 절대 안쓴다
