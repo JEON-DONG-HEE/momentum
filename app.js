4ea6637f905ee0445800984142d747c4a6721365
JEON-DONG-HEE <bethetopone@gmail.com>
2021-06-26 15:45:38 +0900

    210626_04 array test

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-const benchPress = null;
+const mon = "mon";
+const tue = "tue";
+const wed = "wed";
+const thu = "thu";
+const fri = "fri";
+const sat = "sat";
+const sun = "sun";
 
-let something;
-console.log(benchPress, something);
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
 
-// null 은 null 이라는 값을 사용자가 지정한 경우에 볼 수 있고,
-// undefined 는 변수 선언만 되고 값을 지정하지 않은, 즉 메모리에 공간만 확보 해놓고
-// 아직 값을 지정하지 않은 상태이다.
+console.log(daysOfWeek[5]);
 
-// "저희 식사 예약좀 하려구요, 메뉴는 아직 안정했어요" - null
-// "저희 예약좀 하려구요, 술을 할지 식사를 할지는 가서 정할게요 일단 예약만 해주세요" - undefined
+console.log(daysOfWeek);
 
-// 이렇게 이해하려고 하는게 어떨때는 더 헷갈리는 거 같다
+daysOfWeek.push("sun");
+
+console.log(daysOfWeek);
+
+console.log(daysOfWeek[6]);
+
