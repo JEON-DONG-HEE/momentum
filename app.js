33ffec11c2e1e336f13ef23035824a2611b5c904
JEON-DONG-HEE <bethetopone@gmail.com>
2021-06-26 15:17:07 +0900

    210626_03 null 과 undefined 의 차이

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-const a = 50;
-const b = 2;
-let myName = "전동희";
+const benchPress = null;
 
-console.log(a + b);
-console.log(a * b);
-console.log(a / b);
-console.log("My name is " + myName);
+let something;
+console.log(benchPress, something);
 
-myName = "Jhon";
+// null 은 null 이라는 값을 사용자가 지정한 경우에 볼 수 있고,
+// undefined 는 변수 선언만 되고 값을 지정하지 않은, 즉 메모리에 공간만 확보 해놓고
+// 아직 값을 지정하지 않은 상태이다.
 
-console.log("나의 영어 이름은 " + myName);
+// "저희 식사 예약좀 하려구요, 메뉴는 아직 안정했어요" - null
+// "저희 예약좀 하려구요, 술을 할지 식사를 할지는 가서 정할게요 일단 예약만 해주세요" - undefined
 
-// const 를 위주로 쓰고, let 은 가끔 필요할 때 쓰고, var 는 절대 안쓴다
+// 이렇게 이해하려고 하는게 어떨때는 더 헷갈리는 거 같다
