166bc2c7017654e516d24ace788973c4dafb4719
JEON-DONG-HEE <bethetopone@gmail.com>
2021-06-27 15:45:28 +0900

    210627_02 음주 가능 나이 체크

--- app.js
+++ app.js
@@ -1,22 +1,15 @@
-const calculator = {
-    calPlus : function(num1, num2) {
-        console.log('안녕');
-        return num1 + num2;
-        console.log('안녕2');
-    },
-    calMinus : function(num1, num2) {
-        console.log("두 수의 차는 " + (num1 - num2) + " 입니다.")
-    },
-    calDevide : function(num1, num2) {
-        console.log(num1 + " 을 " + num2 + " 로 나눈 값은" + (num1/num2) + " 입니다.")
-    },
-    calMultiply : function(num1, num2) {
-        console.log(num1 + " 와 " + num2 + " 의 곱은" + (num1*num2) + " 입니다.")
-    },
+const age = parseInt(prompt("How old are you?"));
 
-};
+console.log(isNaN(age)); 
 
-const calcPlus = calculator.calPlus(2, 3);
-
-
-
+if (isNaN(age) || age < 1) {
+    console.log("Write a real number")
+} else if (age < 18) {
+    console.log("You are too young");
+} else if (age >=18 && age <=50){
+    console.log("You can Drink");
+} else if (age >= 50 && age <=80) {
+    console.log("You can exercise");
+} else if (age > 80) {
+    console.log("You can do anything");
+}
