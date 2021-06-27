1aa2c1180b78d176bb629a5db8da11d59d22723b
JEON-DONG-HEE <bethetopone@gmail.com>
2021-06-27 23:55:37 +0900

    210627_04 객체 이벤트 연습

--- app.js
+++ app.js
@@ -1,19 +1,56 @@
-const age = parseInt(prompt("How old are you?"));
+const hellos = document.querySelector('div:nth-child(1) .hello');
+const hellos2 = document.querySelector('div:nth-child(2) .hello');
 
-console.log(isNaN(age)); 
+console.log(hellos);
 
-if (isNaN(age) || age < 1) {
-    console.log("Write a real number")
-} else if (age < 18) {
-    console.log("You are too young");
-} else if (age >=18 && age <=50){
-    console.log("You can Drink");
-} else if (age >= 50 && age <=80) {
-    console.log("You can exercise");
-} else if (age === 100) { // 2. 그래서 원하는 결과를 위해 이렇게 순서를 바꿔야 할 때도 있음
-    console.log("You are wise");    
-} else if (age > 80) {
-    console.log("You can do anything");
-} else if (age === 100) {   // 1. 바로 윗줄에서 true 가 나오기 때문에 아랫줄을 실행이 되지 않음
-    console.log("You are wise");    
+hellos.addEventListener("click", changeColor);
+hellos.addEventListener("mouseover", chkMouseOver);
+hellos.onmouseleave = chkMouseLeave;    // 이렇게 사용해도 되지만 나중에 이벤트 리스너를 제거하는 경우도 있으므로 이런식의 방법은 좋은 방법은 아니다
+
+hellos2.addEventListener("focus", chkFocus);
+hellos2.addEventListener("blur", chkBlur);
+
+
+window.addEventListener("resize", resizeDoc);
+window.addEventListener("copy", chkDrag);
+window.addEventListener("beforeprint", chkPrint);
+
+
+
+function changeColor() {
+    hellos.style.color = "red";
 }
+
+function chkMouseOver() {
+    hellos.innerText = "Mouse in";
+}
+
+function chkMouseLeave() {
+    hellos.innerText = "Mouse out";
+}
+
+function chkFocus() {
+    hellos2.value = "포커스 되었습니다";
+}
+
+function chkBlur() {
+    hellos2.value = "";
+}
+
+function resizeDoc() {
+    document.body.style.background = "lightgreen";
+}
+
+function chkDrag() {
+    alert('복사했습니다');
+}
+
+function chkPrint() {
+    alert('문서 출력 중입니다 ^^');
+}
+
+
+
+//console.dir(hellos);  // 이런식으로 객체가 가지고 있는 property 를 검색하고 코드에 사용할 수 있다
+
+
