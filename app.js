81aa97459c44b0cb36693ac8b4f8b42df6e86d64
JEON-DONG-HEE <bethetopone@gmail.com>
2021-06-27 15:59:23 +0900

    210627_03 if문 실행순서 관련

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ if (isNaN(age) || age < 1) {
     console.log("You can Drink");
 } else if (age >= 50 && age <=80) {
     console.log("You can exercise");
+} else if (age === 100) { // 2. 그래서 원하는 결과를 위해 이렇게 순서를 바꿔야 할 때도 있음
+    console.log("You are wise");    
 } else if (age > 80) {
     console.log("You can do anything");
+} else if (age === 100) {   // 1. 바로 윗줄에서 true 가 나오기 때문에 아랫줄을 실행이 되지 않음
+    console.log("You are wise");    
 }
