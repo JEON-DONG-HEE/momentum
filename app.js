6a1721c9559e166eb744a7f36cb60aeee49bb208
JEON-DONG-HEE <bethetopone@gmail.com>
2021-06-28 13:01:05 +0900

    210628_02 toggle() 함수로 on 클래스 추가, 삭제

--- app.js
+++ app.js
@@ -1,25 +1,35 @@
-const hellos = document.querySelector('div:nth-child(1) .hello a');
-
-// console.log(hellos.style.color);
-// hellos.style.color = "lightgreen";
-// console.log(hellos.style.color);
+const titH1 = document.querySelector('div:nth-child(1) h1');
 
 
-
+// function_01 이렇게 하면 기존의 클래스를 잃어버리는 현상이 나타남
+/*
 function handleChangeColor() {
-    const currentColor = hellos.style.color;
-    let newColor;
-    if (currentColor === "lightgreen") {
-        newColor = "tomato";
+    const chkClicked = "sexy_font on";  // 휴먼에러의 가능성을 줄이기 위해 이렇게 상수에 담음(2개 이상일 경우 이렇게 하는게 좋음)
+    if (titH1.className === chkClicked) {
+        titH1.className = "";
     } else {
-        newColor = "lightgreen";
+        titH1.className = chkClicked;
     }
-    hellos.style.color = newColor;
+};
+*/
+
+// function_02 그래서 이렇게 토글 기능을 구현할 수 있음, 하지만 이것도 길다
+/*
+function handleChangeColor() {
+    const chkClicked = "on";
+    if (titH1.classList.contains(chkClicked)) {
+        titH1.classList.remove(chkClicked);
+    } else {
+        titH1.classList.add(chkClicked);
+    }
+};
+*/
+// function_03 이렇게 하면 된다. 제이쿼리 필요없네
+function handleChangeColor() {
+    titH1.classList.toggle("on"); // 하나만 들어가기 때문에 굳이 휴먼에러를 피하기 위해 상수에 담을 필요는 없음
 };
 
 
-
-
-hellos.addEventListener("click", handleChangeColor);
+titH1.addEventListener("click", handleChangeColor);
 
 
