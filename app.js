c36a7ac075f199f772966621c9f75469a5758f3e
JEON-DONG-HEE <bethetopone@gmail.com>
2021-06-29 15:01:43 +0900

    210629_03 preventDefault 로 a 태그의 기본 기능 막기

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const loginForm = document.querySelector("#login_form");
 const loginInput = loginForm.querySelector("#login_form input[type='submit']");
+const linkGoogle = document.querySelector('a');
 
-function onLoginSubmit(e) {
-    e.preventDefault(); // 브라우저의 기본 동작을 막아줌, 이 소스에서는 submit 의 기본 동작(페이지 새로고침, 서버와의 동기화)을 막아줌
-    const userName = loginInput.value;
-    console.log(e);
+function onLoginSubmit(event) {
+    event.preventDefault(); // 브라우저의 기본 동작을 막아줌, 이 소스에서는 submit 의 기본 동작(페이지 새로고침, 서버와의 동기화)을 막아줌
+    console.log(event);
 };
 
-//form 은 이벤트리스너를 버튼이나 submit 버튼에 걸어주는게 아니라 form 자체에 걸어줘야 한다 End point trigger?
-loginForm.addEventListener("submit", onLoginSubmit);  
+function goToLink(event) {
+    event.preventDefault();
+    console.log(event);
+};
+
+loginForm.addEventListener("submit", onLoginSubmit); //form 은 이벤트리스너를 버튼이나 submit 버튼에 걸어주는게 아니라 form 자체에 걸어줘야 한다 End point trigger?      
+linkGoogle.addEventListener("click", goToLink);
+
+
+
+
 
 
