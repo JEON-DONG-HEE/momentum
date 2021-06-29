a242043df829e45e518d54060c06afb28d45bade
JEON-DONG-HEE <bethetopone@gmail.com>
2021-06-29 09:05:00 +0900

    210629_01 input 유효성 체크

--- app.js
+++ app.js
@@ -1,35 +1,19 @@
-const titH1 = document.querySelector('div:nth-child(1) h1');
+const loginForm = document.getElementById("login_form"); // js가 아이디임을 인지하므로 #은 안 넣는다
+const loginInput = loginForm.querySelector("input");
+const loginBtn = loginForm.querySelector("button");
 
-
-// function_01 이렇게 하면 기존의 클래스를 잃어버리는 현상이 나타남
+// 이런식의 validation 을 스크립트로 연습하는 것은 좋은 연습이다.
+// 하지만 항상 사용할 수 있는 최고의 방법을 사용하는 것이 좋으며
+// 이 케이스에서 그것은 HTML 의 validation 관련 속성을 사용하는 것이다.
 /*
-function handleChangeColor() {
-    const chkClicked = "sexy_font on";  // 휴먼에러의 가능성을 줄이기 위해 이렇게 상수에 담음(2개 이상일 경우 이렇게 하는게 좋음)
-    if (titH1.className === chkClicked) {
-        titH1.className = "";
-    } else {
-        titH1.className = chkClicked;
+function clickLoginBtn() {
+    const userName = loginInput.value;
+    if (userName === "") {
+        console.log("이름을 입력하세요");
+    } else if (userName.length > 15) {
+        alert("입력 가능한 글자수를 초과했습니다");
     }
 };
 */
 
-// function_02 그래서 이렇게 토글 기능을 구현할 수 있음, 하지만 이것도 길다
-/*
-function handleChangeColor() {
-    const chkClicked = "on";
-    if (titH1.classList.contains(chkClicked)) {
-        titH1.classList.remove(chkClicked);
-    } else {
-        titH1.classList.add(chkClicked);
-    }
-};
-*/
-// function_03 이렇게 하면 된다. 제이쿼리 필요없네
-function handleChangeColor() {
-    titH1.classList.toggle("on"); // 하나만 들어가기 때문에 굳이 휴먼에러를 피하기 위해 상수에 담을 필요는 없음
-};
-
-
-titH1.addEventListener("click", handleChangeColor);
-
-
+loginBtn.addEventListener("click", clickLoginBtn);
