1fc80a10e6f5464d5ed32f3a7eb30afa3d2ecfa0
JEON-DONG-HEE <bethetopone@gmail.com>
2021-06-30 15:23:08 +0900

    210630_01 로컬스토리지에 유저 정보 저장

--- app.js
+++ app.js
@@ -1,23 +1,33 @@
 const loginForm = document.querySelector("#login_form");
-const loginInput = loginForm.querySelector("#login_form input");
-const loginResult = document.querySelector(".uer_name");
+const loginInput = loginForm.querySelector("#login_form input");    // querySelector 가 검색되는 첫번째 요소만 선택함으로 submit 은 선택되지 않는다
+const loginResult = document.querySelector(".user_name");
 
-const HIDDEN_CLASSNAME = "dp_none"; // 노마드코더는 단순히 string 값을 저장할 때는 대문자로 이렇게 네이밍하는 것이 관습이라고 한다
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
     event.preventDefault();
-    const userName = loginInput.value;
-    
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    console.log(userName);
-    //loginResult.innerText = "Hello " + userName;  // 일반적으로 이렇게 쓰지만
-    loginResult.innerText = `Hello ${userName} 환영합니다`;    // 이렇게 써도 됨(+ 로 연결 안해줘도 됨) 여기서 $는 제이쿼리 아님, 주의해야 할건 ``(백틱) 기호로 넣어야 한다는 것이다
+    const userName = loginInput.value;
+    //console.log(userName);
+    localStorage.setItem("userName", userName);
+    
+    loginResult.innerText = `Hello ${userName} 환영합니다`;     
     loginResult.classList.remove(HIDDEN_CLASSNAME);
+    
 };
 
 loginForm.addEventListener("submit", onLoginSubmit);
 
+const userNameStorage = localStorage.getItem("userName");
 
+//console.log(userNameStorage);
 
-
+if (userNameStorage === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginResult.classList.add(HIDDEN_CLASSNAME);
+} else {
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    loginResult.classList.remove(HIDDEN_CLASSNAME);
+    loginResult.innerText = `Hello ${userNameStorage} 환영합니다`;        
+}
 
