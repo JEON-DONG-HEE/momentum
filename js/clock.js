1ee9d97323d385baa8c6f4786823fc86124d7bf8
JEON-DONG-HEE <bethetopone@gmail.com>
2021-07-01 10:04:47 +0900

    210701_02 시계기능 구현(완성)

--- js/clock.js
+++ js/clock.js
@@ -2,14 +2,15 @@ const clock = document.querySelector(".clock");
 
 function getClock() {
     const nowTime = new Date();
-    const getHour = nowTime.getHours();
-    const getMinute = nowTime.getMinutes();
-    const getSecond = nowTime.getSeconds();
+
+    // padStart() 함수는 String 에 쓸 수 있다 
+    // Date() 함수는 number 를 반환함으로 String() 함수로 변환을 해주어야 padStart() 함수를 사용할 수 있다
+    const getHour = String(nowTime.getHours()).padStart(2, "0");  
+    const getMinute = String(nowTime.getMinutes()).padStart(2, "0");
+    const getSecond = String(nowTime.getSeconds()).padStart(2, "0");
 
     clock.innerText = `${getHour}:${getMinute}:${getSecond}`;
 };
 
 getClock();
 setInterval(getClock, 1000);
-
-// 시계 기능이 구현되었으나 1초가 01초로 나오지 않는 문제점이 있다
