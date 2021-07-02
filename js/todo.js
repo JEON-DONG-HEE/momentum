81de370403a25931211f1eff7a91d328b79ecda1
JEON-DONG-HEE <bethetopone@gmail.com>
2021-07-02 14:27:56 +0900

    210702_02 할일 목록 삭제 기능

--- js/todo.js
+++ js/todo.js
@@ -1,14 +1,22 @@
-const toDoForm = document.querySelector('#todo_form');
-const toDoInput = toDoForm.querySelector('input');
-const toDoList = document.querySelector('.list_todo');
+const toDoForm = document.querySelector("#todo_form");
+const toDoInput = toDoForm.querySelector("input");
+const toDoList = document.querySelector(".list_todo");
 
+function deleteToDo(event) {
+    const li = event.target.parentElement;
+    li.remove();
+}
 
 function paintToDo(toDoValue) {
-    const listItem = document.createElement('li');
-    const spanTxt = document.createElement('span');
-    listItem.appendChild(spanTxt);
-    spanTxt.innerText = toDoValue;
-    toDoList.appendChild(listItem);
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    span.innerText = toDoValue;
+    const button = document.createElement("button");
+    button.innerText = "X";
+    button.addEventListener("click", deleteToDo)
+    li.appendChild(span);
+    li.appendChild(button);
+    toDoList.appendChild(li);
 }
 
 function handleToDoSubmit(event) {
@@ -19,3 +27,6 @@ function handleToDoSubmit(event) {
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+
+
