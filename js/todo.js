29a622df60af1fa7ca1c241db33a8dbe7261045b
JEON-DONG-HEE <bethetopone@gmail.com>
2021-07-03 17:14:27 +0900

    210703_03 할일 목록 완성

--- js/todo.js
+++ js/todo.js
@@ -3,26 +3,32 @@ const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector(".list_todo");
 
 let toDos = []; // 로컬 스토리지의 내용이 변경될 수 있도록 let 으로 변경
+
 const TODOS_KEY = "toDos";
 
 function deleteToDo(event) {
     const li = event.target.parentElement;
     li.remove();
+    toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));    // li.id 는 string 타입이므로 타입 변환을 해주어야 비교할 수 있다
+    saveTodo();
 }
 
 function saveTodo() {
     localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));   // JSON.stringify 는 입력값을 string 으로 변환해 준다
 }
 
-
-// to do 리스트를 추가하는 기능만 있는 함수
+// to do 리스트를 업데이트하는 함수
 function paintToDo(toDoValue) {
     const li = document.createElement("li");
+    li.id = toDoValue.id;
+
     const span = document.createElement("span");
-    span.innerText = toDoValue;
+    span.innerText = toDoValue.text;
+
     const button = document.createElement("button");
     button.innerText = "X";
-    button.addEventListener("click", deleteToDo)
+    button.addEventListener("click", deleteToDo);
+
     li.appendChild(span);
     li.appendChild(button);
     toDoList.appendChild(li);
@@ -32,8 +38,13 @@ function handleToDoSubmit(event) {
     event.preventDefault();
     const toDoValue = toDoInput.value;
     toDoInput.value = "";
-    toDos.push(toDoValue);
-    paintToDo(toDoValue);
+    const newToDoObj = {
+        text : toDoValue,
+        id : Date.now(), 
+    }
+    
+    toDos.push(newToDoObj);
+    paintToDo(newToDoObj);
     saveTodo();
 }
 
